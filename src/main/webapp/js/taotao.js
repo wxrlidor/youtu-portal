23090f8a8876195cf0f034250b97ab0eb0bc9e6e
lidor <648799160@qq.com>
2018-02-25 21:35:07 +0800

    实现门户调用单点登录系统进行登录

--- src/main/webapp/js/taotao.js
+++ src/main/webapp/js/taotao.js
@@ -1,17 +1,20 @@
 var TT = TAOTAO = {
 	checkLogin : function(){
-		var _ticket = $.cookie("TT_TOKEN");
+		//取出cookie中的token，名称为
+		var _ticket = $.cookie("YOUTU_TOKEN");
 		if(!_ticket){
 			return ;
 		}
+		//使用jQuery包装好的jsonp调用
 		$.ajax({
 			url : "http://localhost:8084/user/token/" + _ticket,
 			dataType : "jsonp",
 			type : "GET",
 			success : function(data){
 				if(data.status == 200){
-					var username = data.data.username;
-					var html = username + "，欢迎来到淘淘！<a href=\"http://www.taotao.com/user/logout.html\" class=\"link-logout\">[退出]</a>";
+					var username = data.data.nickname;
+					//退出时调用安全退出
+					var html = username + "，欢迎来到优兔！<a href=\"http://localhost:8084/user/logout/"+ _ticket +"\" class=\"link-logout\">[退出]</a>";
 					$("#loginbar").html(html);
 				}
 			}
